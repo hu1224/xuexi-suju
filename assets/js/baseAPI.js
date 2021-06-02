d86f849e87c381643f365a96b3dc989ea01aa91a
hu1224 <84969798+hu1224@users.noreply.github.com>
2021-06-02 18:27:34 +0800

    主页的登录和退出和权限的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,7 @@
 $.ajaxPrefilter(function(options){
     options.url = 'http://api-breakingnews-web.itheima.net' +options.url 
+    if(options.url.indexOf('/my') !== -1){
+        options.headers = { Authorization:localStorage.getItem('daidl')}
+    }
+    
 })
---
+++ js/index.js
@@ -0,0 +1,53 @@
+$(function(){
+    // 引入方法
+    var layer = layui.layer
+    huoqu();
+    function huoqu(){
+        $.ajax({
+            type:'GET',
+            url:'/my/userinfo',
+            success:function(res){
+                console.log(res);
+                if(res.status == 0){
+                    genxi(res.data)
+                }
+                
+            },
+            complete:function(res){
+                console.log(res);
+                 if(res.responseJSON.status == 1){
+                    location.href = './login.html';
+                }
+            }
+        }) 
+    }
+    function genxi(nuer){
+       if(nuer.nickname !== '') {
+        $('#welcome').html('欢迎 '+nuer.nickname)
+       }else(
+        $('#welcome').html('欢迎 '+nuer.username) 
+       );
+       if (nuer.user_pic !== null){
+           $(".layui-nav-img").prop('str',nuer.user_pic).show();
+           $('.text-avatar').hide()
+       }else{
+            $(".layui-nav-img").hide();
+           $('.text-avatar').html(nuer.username[0].toUpperCase()).show()
+       }
+    }
+    
+
+    //退出登录
+    $('#lokout').on('click',function(){
+        //eg1
+    layer.confirm('确认退出吗？', {icon: 3, title:'提示'}, function(index){
+        location.href = './login.html';
+        localStorage.removeItem('daidl');
+        layer.close(index);
+    });  
+    })
+
+    //设置权限
+
+
+})
